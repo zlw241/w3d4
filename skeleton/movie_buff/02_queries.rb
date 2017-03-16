fdf5cc9ff2af782b8674f5ca14f3613307c1c128
Zach Ward <=>
2017-03-16 11:56:54 -0400

    Initial commit

---
+++ skeleton/movie_buff/02_queries.rb
@@ -0,0 +1,40 @@
+def eighties_b_movies
+  # List all the movies from 1980-1989 with scores falling between 3 and 5 (inclusive).
+  # Show the id, title, year, and score.
+  Movie.where(yr: 1980..1989,  score: 3..5).select(:id, :title, :yr, :score)
+end
+
+def bad_years
+  # List the years in which a movie with a rating above 8 was not released.
+  Movie.group(:yr)
+    .having("MAX(score) < 8")
+    .order(:yr)
+    .pluck(:yr)
+end
+
+def cast_list(title)
+  # List all the actors for a particular movie, given the title.
+  # Sort the results by starring order (ord). Show the actor id and name.
+  Actor.joins(:movies).order("castings.ord").where("movies.title = (?)", title)
+end
+
+def vanity_projects
+  # List the title of all movies in which the director also appeared as the starring actor.
+  # Show the movie id and title and director's name.
+
+  # Note: Directors appear in the 'actors' table.
+  Actor.joins(:movies)
+    .where("castings.ord = 1 AND movies.director_id = actors.id")
+    .select("movies.id, movies.title, actors.name")
+end
+
+def most_supportive
+  # Find the two actors with the largest number of non-starring roles.
+  # Show each actor's id, name and number of supporting roles.
+  Actor.joins(:movies)
+  .where("castings.ord > 1")
+  .order("COUNT(castings.ord) DESC")
+  .group("actors.id")
+  .limit(2)
+  .select("actors.id, actors.name, COUNT(castings.ord) AS roles")
+end
---
+++ skeleton/movie_buff/03_queries.rb
@@ -0,0 +1,53 @@
+def what_was_that_one_with(those_actors)
+  # Find the movies starring all `those_actors` (an array of actor names).
+  # Show each movie's title and id.
+  # Movie.joins(:actors)
+  #   .where("actors.name IN (?)", those_actors)
+  #   .select(:id, :title)
+  #   .order("movies.id DESC")
+  #   .uniq
+
+  Actor.joins(:movies)
+    .where("actors.name IN (?)", those_actors)
+    .select("movies.id, movies.title")
+    .group("movies.id")
+    .having("COUNT(actors.id) = ?", those_actors.length)
+
+
+end
+
+def golden_age
+  # Find the decade with the highest average movie score.
+  Movie.select("(movies.yr / 10) * 10 as decade, AVG(movies.score)")
+  .group("decade")
+  .order("AVG(movies.score) DESC")
+  .limit(1)
+  .first
+  .decade
+end
+
+def costars(name)
+  # List the names of the actors that the named actor has ever appeared with.
+  # Hint: use a subquery
+  
+end
+
+def actor_out_of_work
+  # Find the number of actors in the database who have not appeared in a movie
+
+end
+
+def starring(whazzername)
+  # Find the movies with an actor who had a name like `whazzername`.
+  # A name is like whazzername if the actor's name contains all of the letters in whazzername,
+  # ignoring case, in order.
+
+  # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but not like "stallone sylvester" or "zylvester ztallone"
+end
+
+def longest_career
+  # Find the 3 actors who had the longest careers
+  # (the greatest time between first and last movie).
+  # Order by actor names. Show each actor's id, name, and the length of their career.
+
+end
